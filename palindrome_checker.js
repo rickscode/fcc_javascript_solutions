1f0213182b62ae17b1984da715a35b60952db374
“Ricky <grickyh@gmail.com>
2021-01-27 16:13:25 +0700

    Cleaned up code and comments

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -1,11 +1,20 @@
 function palindrome(str) {
-  //replace underscores and non word characters
-  //make all lower case
-  //reverse string to new variable
-  //compare the strings
+  
+  // Replace underscores and non word characters
+  
   let noSpaceStr = str.replace(/[\W_]/g, "");
+  
+  // Make all lower case
+
   let lowerCaseStr = noSpaceStr.toLowerCase();
+
+  // Reverse string to new variable
+
   let reverseStr = '';
+  
+  // For loop to compare the strings 
+
+
   for(let i = lowerCaseStr.length -1; i >= 0; i--){
     reverseStr += lowerCaseStr[i];
   }
